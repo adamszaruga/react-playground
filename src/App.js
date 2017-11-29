6d0dbed5e1ccaa742c0209ca0366d895402f4abe
Adam Szaruga <adamszaruga@gmail.com>
2017-11-28 21:46:38 -0500

    really quick search results app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
+var searchResults = [
+	{
+		title: "dark knight"
+	},
+	{
+		title: "dork knight"
+	},
+	{
+		title: "dark night"
+	}
+]
+
+
+var inputStyle = {
+	width: "300px"
+}
+
+let SearchResult = (props) => {
+	return <p>{props.title}</p>
+}
+
+
 let App = ()=>{
-  return <h1>Write your JSX here! (App.js, line 5)</h1>
+  var arrayOfSearchElements = [];
+  for (var i=0; i<searchResults.length; i++ ) {
+  	arrayOfSearchElements.push(<SearchResult title={searchResults[i].title}/>)
+  }
+
+  return (
+  	<div>
+	  	<input style={inputStyle} />
+	  	<hr />
+	  	{arrayOfSearchElements}
+  	</div>
+  )
 }
 
 export default App;
